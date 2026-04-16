e9cd28e8fb079fbb662e45adf0fe4ffdcb3ed2f3
BadOfficer <tarasbondarenko2005@gmail.com>
2026-04-16 22:59:47 +0300

    Solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -7,9 +7,51 @@ describe(`Function 'checkPassword':`, () => {
     expect(checkPassword).toBeInstanceOf(Function);
   });
 
-  it(`should return boolean`, () => {});
+  it(`should return boolean`, () => {
+    const result = checkPassword('Password1!');
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {});
+    expect(typeof result).toBe('boolean');
+  });
 
-  // write more tests here
+  it(`should return 'true' for the valid password with 8 characters`, () => {
+    const result = checkPassword('Password1!');
+
+    expect(result).toBe(true);
+  });
+
+  it(`should return 'false' for password with length less than 8 chars`, () => {
+    const result = checkPassword('Q!1wert');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for password with length great that 16 chars`, () => {
+    const result = checkPassword('Q!1wertdvdsvsdvsdvsdvsdvdsv');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for password contains not Lating chars`, () => {
+    const result = checkPassword('А!123СБАЛВ');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for password not contained digit`, () => {
+    const result = checkPassword('Str@ngaaa');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for password not contained special char`, () => {
+    const result = checkPassword('Str12ngaaa');
+
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for password not contained uppercase letter`, () => {
+    const result = checkPassword('str@nga123');
+
+    expect(result).toBe(false);
+  });
 });
